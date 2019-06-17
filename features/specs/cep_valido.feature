beacbe6d1ec86c285a28236c6760da6b6e9485ec
bruno.santos.ext <bruno.santos.ext@conductor.com.br>
2019-06-17 12:03:02 -0300

    subindo alteração de teste

--- features/specs/cep_valido.feature
+++ features/specs/cep_valido.feature
@@ -4,11 +4,11 @@ Funcionalidade: Valida Cep
 
 @valido
 Cenário: Válidar CEP válido pela API dos correios
-  Dado que faça uma consulta atraves de um CEP
-  Então o sistema retorna as informações da pesquisa
+  Dado que faça uma consulta atraves de um CEP valido
+  Então o sistema retorna as informações da pesquisa e valor do ibge
   E o código da resposta deve ser "200"
 
 @invalido
   Cenário: Válidar CEP Inválido pela API dos correios
     Dado que faça uma consulta atraves de um CEP inválido
-    Então o código da resposta deve ser "404"
+    Então o código da resposta deve ser "400"
--- features/step_definitions/validar_cep.rb
+++ features/step_definitions/validar_cep.rb
@@ -1,27 +1,16 @@
-Dado(/^que faça uma consulta atraves de um CEP$/) do
+Dado(/^que faça uma consulta atraves de um CEP valido$/) do
   @result = HTTParty.get('https://viacep.com.br/ws/06322010/json')
 end
 
 Dado(/^que faça uma consulta atraves de um CEP inválido$/) do
-  @result = HTTParty.get('https://viacep.com.br/ws/99999999/json')
+  @result = HTTParty.get('https://viacep.com.br/ws/000/json')
   puts @result.response.code
 end
 
-Então(/^o sistema retorna as informações da pesquisa$/) do
+Então(/^o sistema retorna as informações da pesquisa e valor do ibge$/) do
   resultado = @result.parsed_response
   expect(resultado).not_to be_empty
-  puts resultado
-end
-
-Quando(/^tiver um cep cadastrado$/) do
- @cep = {
-   :cep => "06322010",
-   :tipoDeLogradouro => "Avenida",
-   :logradouro => "Eugênia",
-   :bairro => "Centro",
-   :cidade => "Carapicuíba",
-   :estado => "SP"
- }
+  puts ibge = JSON.parse(resultado)['ibge']
 end
 
 Então(/^o código da resposta deve ser "([^"]*)"$/) do |arg1|
